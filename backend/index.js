eb9cd45ea4804e55591db76baa6ed78357dbdec4
ROMAN;) <romanavilezuw@gmail.com>
2025-12-08 20:35:52 -0800

    Add GameCard.jsx

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./src/lib/db.js";
-import gameRoutes from './src/routes/games.js';
-import gamerRoutes from './src/routes/gamers.js';
-import reviewRoutes from './src/routes/reviews.js';
+import gameRoutes from './src/routers/games.js';
+import gamerRoutes from './src/routers/gamers.js';
+import reviewRoutes from './src/routers/reviews.js';
 
 const app = express();
 
-app.use(cors({origin: "http://localhost:5173"}));
+app.use(cors());
 
 app.use("/api/games", gameRoutes);
 app.use("/api/gamers", gamerRoutes);
